2cbb9bf1a639e4a60f9c0edda1e87101d609aeb0
FranklinGitHub <fcandurin914@e.uneg.edu.ve>
2025-03-09 00:51:58 -0400

    File update and modification

--- server/index.ts
+++ server/index.ts
@@ -58,12 +58,10 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  const PORT = parseInt(process.env.PORT || "5000", 10);
+  const HOST = 'localhost';
+
+  server.listen(PORT, HOST, () => {
+    log(`Server is running on http://${HOST}:${PORT}`);
   });
 })();
