dfb4e94c3c1b8c33d19ea5ef17ffe9b12b2c8acf
FranklinGitHub <fcandurin914@e.uneg.edu.ve>
2025-03-09 22:57:34 -0400

    Updating configuration files

---
+++ server/db.ts
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const uri = process.env.MONGODB_URI || 'mongodb+srv://fcandurin914:tVcx3w401jA19ufX@cluster-ecosynthai.ugktc.mongodb.net/';
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+  }
+}
+
+export { client, connectDB };
--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes.js";
 import { setupVite, serveStatic, log } from "./vite.js";
+import { connectDB } from './db';
 
 console.log('SESSION_SECRET:', process.env.SESSION_SECRET);
 
@@ -41,6 +42,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  await connectDB();
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
